3e076fb935fb97f914aeb70c9577c35f202ebd83
cheolmin choi <zl0759vk@naver.com>
2020-12-24 18:29:59 +0900

    Add Generic

--- src/reat-sheet.ts
+++ src/reat-sheet.ts
@@ -5,8 +5,8 @@ const {
   utils: { sheet_to_json },
 } = XLSX;
 
-export const readFirstSheet = (data: any, options?: XLSX.ParsingOptions) => {
-  const wb: XLSX.WorkBook = read(data, options);
+export const readSheet = <T extends unknown>(xlsxBuffer: any, options?: XLSX.ParsingOptions): T[] => {
+  const wb: XLSX.WorkBook = read(xlsxBuffer, options);
   const ws: XLSX.WorkSheet = wb.Sheets[wb.SheetNames[0]];
   return sheet_to_json(ws);
 };
